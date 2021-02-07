abb1d7878ea31c3921f44adaac5f71a7e543ea0d
gbanm <gbanm06@gmail.com>
2021-02-07 16:32:42 +0100

    test

--- BacklundTransformAnalytics/Program.cs
+++ BacklundTransformAnalytics/Program.cs
@@ -15,26 +15,53 @@ namespace BacklundTransformAnalytics
     public class Program
     {
 
+
+        public class MyCustomBlock : IBlock
+        {
+            public override object Evaluate(Context context)
+            {
+
+                var myField = this.Fields.Get("MY_FIELD");
+
+
+                var myValue = this.Values.Evaluate("MY_VALUE", context);
+
+
+                var myStatement = this.Statements.Get("MY_STATEMENT");
+                myStatement.Evaluate(context);
+
+
+                base.Evaluate(context);
+                return null;
+            }
+        }
+
         internal class CustomPrintBlock : IBlock
         {
             public List<string> Text { get; set; } = new List<string>();
 
+            public Func<double,double> Func { get; set; }
+
             public override object Evaluate(Context context)
             {
+               
                 Text.Add((this.Values.FirstOrDefault(x => x.Name == "TEXT")?.Evaluate(context) ?? "").ToString());
                 return base.Evaluate(context);
             }
         }
+  
         [JSInvokable]
         public static string Evaluate(string xml)
         {
             var printBlock = new CustomPrintBlock();
-
+            var customBlock = new MyCustomBlock();
             new Parser()
-              .AddStandardBlocks()
-              .AddBlock("text_print", printBlock)
-              .Parse(xml)
-              .Evaluate();
+        .AddStandardBlocks()
+        .AddBlock("text_print", printBlock).AddBlock("procedures_defreturn", customBlock)
+        .Parse(xml)
+        .Evaluate();
+
+
 
             return string.Join('\n', printBlock.Text);
 
